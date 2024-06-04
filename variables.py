9fe0ccbded3bec649cf05bb73cc5ef046b123000
Roman Mendoza Niella <165208834+menroq@users.noreply.github.com>
2024-06-04 19:54:31 +0200

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,23 @@
+# TASK1 
+
+name = "Ax2"
+print(name)
+# 
+
+# TASK2
+name1 = "John"
+name2 = "Slash"
+name3 = "Duff"
+name4 = "Izzy"
+
+print(name1 + " and " + name2 + " and ")
+
+
+# TASK3
+
+
+
+
+
+
+# TASK4
